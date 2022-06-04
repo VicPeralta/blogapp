f5fad670ad0b74c31646c83db2ff05f3d8a99574
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-06-04 10:33:06 -0500

    Add model classes comment, like, post and users. Change user controller to use those classes.
    Users views updated to use new classes
    
    'modified:   app/controllers/users_controller.rb
    'new file:   app/models/comment.rb
    'new file:   app/models/like.rb
    'new file:   app/models/post.rb
    'new file:   app/models/user.rb
    'deleted:    app/models/user_info.rb
    'modified:   app/views/users/index.html.erb
    'modified:   app/views/users/show.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,18 +5,12 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users.push(UserInfo.new(1, 'Homer', 1, 'homer.jpg'))
-    @users.push(UserInfo.new(2, 'Marge', 2, 'marge.jpg'))
-    @users.push(UserInfo.new(3, 'Bart', 2, 'bart.gif'))
-    @users.push(UserInfo.new(4, 'Lisa', 10, 'lisa.png'))
+    @users = User.all
   end
 
   def show
-    @users.push(UserInfo.new(1, 'Homer', 1, 'homer.jpg'))
-    @users.push(UserInfo.new(2, 'Marge', 2, 'marge.jpg'))
-    @users.push(UserInfo.new(3, 'Bart', 2, 'bart.gif'))
-    @users.push(UserInfo.new(4, 'Lisa', 10, 'lisa.png'))
-    index = params['id'].to_i - 1
-    @user_info = @users[index]
+    index = params['id'].to_i
+    @user_info = User.find(index)
+    @posts = Post.where(author_id: index)
   end
 end
