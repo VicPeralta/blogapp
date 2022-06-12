7ddff54ee134dcf4ddf14722d2e23e3eb7cbfe67
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-06-12 12:49:26 -0500

    Improves style for forms and flash messages
    Solve n + 1 problem when rendering comments
    
    modified:   app/assets/stylesheets/application.css
    modified:   app/assets/stylesheets/forms.css
    modified:   app/controllers/post_controller.rb
    modified:   app/controllers/posts_controller.rb
    modified:   app/controllers/service_controller.rb
    modified:   app/views/layouts/application.html.erb
    modified:   app/views/post/new.html.erb
    modified:   app/views/posts/show.html.erb
    modified:   app/views/shared/_add_comment.html.erb

--- app/controllers/service_controller.rb
+++ app/controllers/service_controller.rb
@@ -15,7 +15,7 @@ class ServiceController < ApplicationController
     post = Post.find(params[:post_id])
     text = params[:text]
     if text == ''
-      redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id]), notice: 'Empty comment'
+      redirect_to user_post_path(user_id: params[:user_id], id: params[:post_id]), alert: 'Empty comment'
       return
     end
     Comment.create(author: @current_user, post: post, text: text)
