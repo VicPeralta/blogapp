5cb92a8f0e2d866907d8a3aba4e0d4f56aa2297d
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-14 00:46:14 +0430

    Setup devise gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  # before_action :update_allowed_parameters, if: :devise_controller?
+  
   def route_not_found
     render file: "#{Rails.root}/public/404.html", status: 404
   end
 
-  def current_user
-    User.first
-  end
 
-  def self.current_user
-    User.first
-  end
 end
