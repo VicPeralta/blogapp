813b3169eff0d78905adcd2e518e78449db564d2
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-06-13 18:37:46 -0500

    Improve styling. Fix error when requesting to change password.
    modified:   app/assets/stylesheets/application.css
    modified:   app/assets/stylesheets/forms.css
    modified:   app/controllers/application_controller.rb
    modified:   app/views/devise/confirmations/new.html.erb
    modified:   app/views/devise/passwords/edit.html.erb
    modified:   app/views/devise/passwords/new.html.erb
    modified:   app/views/devise/registrations/edit.html.erb
    modified:   app/views/devise/registrations/new.html.erb
    modified:   app/views/devise/sessions/new.html.erb
    modified:   app/views/layouts/application.html.erb
    modified:   db/seeds.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name bio photo])
   end
 end
