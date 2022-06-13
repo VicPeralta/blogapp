265f453be8122816c7e607ab3bcf174353ea0e6c
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-14 01:17:43 +0430

    Resolve turbo issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  # before_action :update_allowed_parameters, if: :devise_controller?
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def route_not_found
     render file: "#{Rails.root}/public/404.html", status: 404
   end
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
 end
