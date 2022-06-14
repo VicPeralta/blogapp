d376cd102177ba0dfb491b906b44efd3f05cb924
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-06-13 19:17:25 -0500

    Add mailer options to production enviroment
    modified:   config/environments/development.rb
    modified:   config/environments/production.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,7 +50,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'https://safe-brook-54426.herokuapp.com/', port: 80 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
