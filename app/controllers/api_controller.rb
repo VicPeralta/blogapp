71d0f0400f0dbb83850805c94edc09ab2e457442
behnam1369 <behnam.aghaali@yahoo.com>
2022-06-17 01:07:27 +0430

    Add api endpoints and required routes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,5 @@
 class ApiController < ApplicationController
+  protect_from_forgery with: :null_session
   skip_before_action :authenticate_user!
 
   def validate_credentials(credential)
@@ -10,16 +11,43 @@ class ApiController < ApplicationController
     true
   end
 
-  def show_api_users
+  def show_users_posts
     respond_to :json
     token = params[:token]
-    user_id = params[:user_id]
+    author_id = params[:author_id]
     credential = ApiHelper::JsonWebToken.decode(token)[0]
     if validate_credentials(credential)
-      puts @current_user.name
-      posts = Post.where(author_id: user_id)
-      puts posts
-      render json: { message: "Welcome #{@current_user.name}" }
+      posts = Post.where(author_id: author_id)
+      render json: posts
+    else
+      render json: { error: 'unauthorized' }
+    end
+  end
+
+  def show_posts_comments
+    respond_to :json
+    token = params[:token]
+    post_id = params[:post_id]
+    credential = ApiHelper::JsonWebToken.decode(token)[0]
+    if validate_credentials(credential)
+      comments = Comment.where(post_id: post_id)
+      puts comments
+      render json: comments
+    else
+      render json: { error: 'unauthorized' }
+    end
+  end
+
+  def add_comment_to_post
+    # puts 'Hellooooooooooooooooooooooooooooooooooo'
+    respond_to :json
+    token = params[:token]
+    post_id = params[:post_id]
+    comment_text = params[:text]
+    credential = ApiHelper::JsonWebToken.decode(token)[0]
+    if validate_credentials(credential)
+      Comment.create(post_id: post_id, author: @current_user, text: comment_text)
+      render json: { message: 'your comment saved successfully' }
     else
       render json: { error: 'unauthorized' }
     end
