ec66545e2a3554e48a7609422c62687b62a080bb
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-06-16 13:59:13 -0500

    Add route to api show user
    modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show', to: 'api#show_api_users', as: 'api_show'
   devise_for :users
   root 'users#index'
   get '/users/:user_id/posts', to:'posts#index', as: 'user_posts'
