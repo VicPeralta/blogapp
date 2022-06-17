24ef659e01427e149621eafa7f37146447ff7693
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-06-17 16:45:08 -0500

    Fixed linters
    modified:   spec/requests/api/api_controller.rb_spec.rb
    modified:   spec/requests/api/my_spec.rb
    modified:   spec/swagger_helper.rb

--- spec/requests/api/api_controller.rb_spec.rb
+++ spec/requests/api/api_controller.rb_spec.rb
@@ -1,4 +1,1 @@
 require 'swagger_helper'
-
-RSpec.describe 'api/api_controller.rb', type: :request do
-end
--- spec/requests/api/my_spec.rb
+++ spec/requests/api/my_spec.rb
@@ -9,28 +9,29 @@ RSpec.describe 'Testint API', type: :request do
       produces 'application/json'
       parameter name: :author_id, in: :path, type: :string
       parameter name: :token, in: :body, schema: {
-        type: :object,
-        properties: {
-          token: { type: :string }
-        },
-        required: ['token']
+        type: :object, properties: { token: { type: :string } }, required: ['token']
       }
       response '200', 'blog found' do
         schema type: :array,
-          properties: {
-              author_id: { type: :integer },
-              title: { type: :string },
-              text: { type: :string },
-              commentsCounter: { type: :integer },
-              likesCounter: { type: :integer },
-              created_at: { type: :date },
-              updated_at: { type: :date },
-              id: { type: :integer }
-            },
-            required: [ 'id' ]
+               properties: {
+                 author_id: { type: :integer },
+                 title: { type: :string },
+                 text: { type: :string },
+                 commentsCounter: { type: :integer },
+                 likesCounter: { type: :integer },
+                 created_at: { type: :date },
+                 updated_at: { type: :date },
+                 id: { type: :integer }
+               },
+               required: ['id']
 
         let(:author_id) { 1 }
-        let(:token) { { token: "eyJhbGciOiJIUzI1NiJ9.ImJlaG5hbS5hZ2hhYWxpQHlhaG9vLmNvbSI.EMSaznbIWMcFfu5VfvmTtfMQ39zWqgGM31UG36jIYEo" }}
+        # Token victorperaltagomez@gmail.com
+        # eyJhbGciOiJIUzI1NiJ9.InZpY3RvcnBlcmFsdGFnb21lekBnbWFpbC5jb20i.n3LHMWVU-kyXYdBpRRtIf1vr2bfmCemmwqTDihNrnwE
+        let(:token) do
+          { token: 'eyJhbGciOiJIUzI1NiJ9.ImJlaG5hbS5hZ2hhYWxpQHlhaG9vLmNvbSI.'\
+                   'EMSaznbIWMcFfu5VfvmTtfMQ39zWqgGM31UG36jIYEo' }
+        end
         run_test!
       end
     end
